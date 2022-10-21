460fd8ff57b1389fb4771038ab330f846765d96d
JasonMRash <jasonmichaelrash@gmail.com>
2022-10-21 16:13:10 -0500

    add get and post organizations and requirements

--- organizations.js
+++ organizations.js
@@ -31,6 +31,15 @@ async function get_requirements(req, id) {
     }
 }
 
+async function post_organization(req, name, initial_required, num_boosters) {
+    var key = datastore.key(ORGANIZATIONS);
+    var requirements = [];
+    const new_organization = {"name": name, "initial_required": initial_required, "num_boosters": num_boosters, "requirements": requirements};
+    await datastore.save({ "key": key, "data": new_organization });
+    var self_url = "/" + req.baseUrl + "/" + key.id;
+    return { "id": key.id, "name": name, "initial_required": initial_required, "num_boosters": num_boosters, "requirements": requirements, "self": self_url };
+}
+
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Controller Functions ------------- */
@@ -53,6 +62,21 @@ router.get('/:id', function (req, res) {
                 return res.status(200).json(organization[0]);
             }
         });
+});
+
+//POST organizations/
+router.post('/:id', function (req, res) {
+    if (req.body.name === undefined || req.body.name === null ||
+        req.body.initial_required === undefined || req.body.initial_required === null ||
+        req.body.num_boosters === undefined || req.body.num_boosters === null) {
+        res.status(400).json({'Error': 'The request object is missing at least one of the required attributes.'});
+    }
+    else {
+        post_organization(req, req.body.name, req.body.initial_required, req.body.num_boosters)
+        .then(organization => {
+            res.status(201).json(organization);
+        })
+    } 
 });
 
 /* ------------- End Controller Functions ------------- */
--- requirements.js
+++ requirements.js
@@ -11,7 +11,7 @@ const REQUIREMENTS = "REQUIREMENTS";
 
 /* ------------- Begin Model Functions ------------- */
 
-async function get_requirements(req, id) {
+async function get_requirement(req, id) {
     const key = datastore.key([REQUIREMENTS, parseInt(id, 10)]);
     const entity = await datastore.get(key);
     if (entity === undefined || entity[0] === null) {
@@ -21,11 +21,58 @@ async function get_requirements(req, id) {
     }
 }
 
+async function get_requirements(req) {
+    var results = {};
+    const q = datastore.createQuery(REQUIREMENTS);
+    return datastore.runQuery(q).then((entities) => {
+        results = entities[0].map(ds.fromDatastore);
+        for (var i = 0; i < results.length; i++) {
+            var self_url = '/requirements/' + results[i].id;
+            results[i].self = self_url;
+        }
+        return {"requirements": results};
+    });
+}
+
+function post_requirement(req, days_max, days_min, quantity, type) {
+    var key = datastore.key(REQUIREMENTS);
+    var organization_id = null;
+    const new_requirement = {"days_max": days_max, "days_min": days_min, "quantity": quantity, "type": type, "organization_id": organization_id};
+    return datastore.save({"key":key, "data":new_requirement}.then(() => {
+        var self_url = "/requirements/" + key.id;
+        return {"id": key.id, "max_days": max_days, "min_days": min_days, "quantity": quantity, "type": type, "organization_id": organization_id, "self": self_url};
+    }));
+}
+
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Controller Functions ------------- */
 
+router.get('/:id', function (req, res) {
+    get_requirement(req, req.params.id)
+    .then(requirement => {
+        if (requirement[0] === undefined || requirement[0] === null) {
+            res.status(404).json({'Error': 'No requirement with this requirement ID exists'});
+        } else {
+            res.status(200).json(requirement[0]);
+        }
+    });
+});
 
+router.post('/:id', function (req, res) {
+    if (req.body.days_max === undefined || req.body.days_max === null ||
+        req.body.days_min === undefined || req.body.days_min === null ||
+        req.body.quantity === undefined || req.body.quantity === null ||
+        req.body.type === undefined || req.body.type === null) {
+            res.status(400).json({'Error': 'The request object is missing at least one of the required attributes'});
+    }
+    else {
+        post_requirement(req, req.body.days_max, req.body.days_min, req.body.quantity, req.body.type)
+        .then(requirement => {
+            res.status(201).send(requirement);
+        });
+    }
+});
 
 /* ------------- End Controller Functions ------------- */
 
