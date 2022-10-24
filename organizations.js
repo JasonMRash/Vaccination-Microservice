e3247561e10cb0bce7ff7851c267ad8c9f9b1ccb
JasonMRash <jasonmichaelrash@gmail.com>
2022-10-24 16:57:37 -0500

    Working on assign requirement to organization

--- organizations.js
+++ organizations.js
@@ -21,7 +21,20 @@ async function get_organization(req, id) {
     }
 }
 
-async function get_requirements(req, id) {
+async function get_organizations(req) {
+    var results = {};
+    const q = datastore.createQuery(ORGANIZATIONS);
+    return datastore.runQuery(q).then((entities) => {
+        results = entities[0].map(ds.fromDatastore);
+        for (var i = 0; i < results.length; i++) {
+            var self_url = '/organizations/' + results[i].id;
+            results[i].self = self_url;
+        }
+        return {"organizations": results};
+    });
+}
+
+async function get_requirement(req, id) {
     const key = datastore.key([REQUIREMENTS, parseInt(id, 10)]);
     const entity = await datastore.get(key);
     if (entity === undefined || entity[0] === null) {
@@ -34,10 +47,33 @@ async function get_requirements(req, id) {
 async function post_organization(req, name, initial_required, num_boosters) {
     var key = datastore.key(ORGANIZATIONS);
     var requirements = [];
-    const new_organization = {"name": name, "initial_required": initial_required, "num_boosters": num_boosters, "requirements": requirements};
+    const new_organization = {"name": name, "initial_required": initial_required, "num_boosters": num_boosters, 
+        "requirements": requirements};
     await datastore.save({ "key": key, "data": new_organization });
     var self_url = "/" + req.baseUrl + "/" + key.id;
-    return { "id": key.id, "name": name, "initial_required": initial_required, "num_boosters": num_boosters, "requirements": requirements, "self": self_url };
+    return { "id": key.id, "name": name, "initial_required": initial_required, "num_boosters": num_boosters, 
+        "requirements": requirements, "self": self_url };
+}
+
+async function delete_organization(req, id) {
+    const key = datastore.key([ORGANIZATIONS, parseInt(id, 10)]);
+    return datastore.delete(key);
+}
+
+async function remove_owner_from_requirement(requirement_id) {
+    const key = datastore.key([REQUIREMENTS, parseInt(requirement_id, 10)]);
+    return datastore.get(key).then(async requirement => {
+        if (requirement[0] === undefined || requirement[0] === null) {
+            return requirement;
+        }
+        else {
+            var owner = null;
+            const remove_owner = {"category": requirement[0].category, "days_max": requirement[0].days_max,
+                "days_min": requirement[0].days_min, "quantity": requirement[0].quantity, "type": requirement[0].type, 
+                "organization_id": owner};
+            return await datastore.update({"key": key, "data": remove_owner});
+        }
+    });
 }
 
 /* ------------- End Model Functions ------------- */
@@ -51,20 +87,20 @@ router.get('/:id', function (req, res) {
             if (organization[0] === undefined || organization[0] === null) {
                 return res.status(404).json({ 'Error': 'No organization with this organization ID exists' });
             } else {
-                get_requirements(req, req.params.id)
-                    .then (requirements => {
-                        if (requirements[0] === undefined || requirements[0] === null) {
-                            array.forEach(element => {
-                                organization[0].requirements.push(element)
-                            });
-                        }
-                    })
                 return res.status(200).json(organization[0]);
             }
         });
 });
 
-//POST organizations/
+// GET organizations/
+router.get('/', function (req, res) {
+    const organizations = get_organizations(req)
+    .then( organizations => {
+        res.status(200).json(organizations);
+    });
+});
+
+// POST organizations/
 router.post('/:id', function (req, res) {
     if (req.body.name === undefined || req.body.name === null ||
         req.body.initial_required === undefined || req.body.initial_required === null ||
@@ -79,6 +115,41 @@ router.post('/:id', function (req, res) {
     } 
 });
 
+// PUT organizations/:organization_id/requirements/:requirement_id
+router.put('/:organization_id/requirements/:requirement_id', function (req, res) {
+    get_organization(req, req.params.organization_id). then( async organization => {
+        if (organization[0] === undefined || organization[0] === null) {
+            res.status(404).json({'Error': 'No organization with that organization_id exists.'});
+        }
+        else {
+            get_requirement(req, req.params.requirement_id).then (async requirement => {
+                if (requirement[0] === undefined || requirement[0] === null) {
+                    res.status(404).json({'Error': 'No requirement with that requirement_id exists'});
+                }
+                else {
+                    await assign_requirement_to_organization(req.params.organization_id, req.params.requirement_id);
+                    res.status(204).end();
+                }
+            })
+        }
+    });
+});
+
+// DELETE organizations/:id
+router.delete('/:id', function (req, res) {
+    get_organization(req, req.params.id). then( async organization => {
+        if (organization[0] === undefined || organization[0] === null) {
+            res.status(404).json({'Error': 'No organization with that organization_id exists.'});
+        }
+        else
+        {
+            for (i = 0; i< organization[0].requirements.length; i++) {
+                await remove_owner_from_requirement(organization[0].requirement[i].id);
+            }
+            await delete_organization(req.params.id).then (res.status(204).send("No Content"));
+        }
+    });
+});
 /* ------------- End Controller Functions ------------- */
 
 module.exports = router;
--- requirements.js
+++ requirements.js
@@ -11,6 +11,7 @@ const REQUIREMENTS = "REQUIREMENTS";
 
 /* ------------- Begin Model Functions ------------- */
 
+
 async function get_requirement(req, id) {
     const key = datastore.key([REQUIREMENTS, parseInt(id, 10)]);
     const entity = await datastore.get(key);
@@ -34,14 +35,61 @@ async function get_requirements(req) {
     });
 }
 
-function post_requirement(req, days_max, days_min, quantity, type) {
+async function get_organization(req, id) {
+    const key = datastore.key([ORGANIZATIONS, parseInt(id, 10)]);
+    const entity = await datastore.get(key);
+    if (entity === undefined || entity[0] === null) {
+        return entity;
+    } else {
+        return entity.map(ds.fromDatastore);
+    }
+}
+
+async function get_organizations(req) {
+    var results = {};
+    const q = datastore.createQuery(ORGANIZATIONS);
+    return datastore.runQuery(q).then((entities) => {
+        results = entities[0].map(ds.fromDatastore);
+        for (var i = 0; i < results.length; i++) {
+            var self_url = '/organizations/' + results[i].id;
+            results[i].self = self_url;
+        }
+        return {"organizations": results};
+    });
+}
+
+function post_requirement(req, category, days_max, days_min, quantity, type) {
     var key = datastore.key(REQUIREMENTS);
     var organization_id = null;
-    const new_requirement = {"days_max": days_max, "days_min": days_min, "quantity": quantity, "type": type, "organization_id": organization_id};
+    const new_requirement = {"category": category, "days_max": days_max, "days_min": days_min, "quantity": quantity, "type": type, "organization_id": organization_id};
     return datastore.save({"key":key, "data":new_requirement}.then(() => {
         var self_url = "/requirements/" + key.id;
-        return {"id": key.id, "max_days": max_days, "min_days": min_days, "quantity": quantity, "type": type, "organization_id": organization_id, "self": self_url};
+        return {"id": key.id, "category": category, "max_days": max_days, "min_days": min_days, "quantity": quantity, "type": type, "organization_id": organization_id, "self": self_url};
     }));
+}
+
+async function remove_requirement_from_organization(organization_id, requirement_id) {
+    const key = datastore.key([ORGANIZATIONS, parseInt(organization_id, 10)]);
+    return datastore.get(key).then(async organization => {
+        if (organization[0] === undefined || organization[0] === null) {
+            return organization;
+        }
+        else {
+            for (var i = 0; i < organization[0].requirements.length; i++) {
+                if (organization[0].requirements[i].id == requirement_id) {
+                    organization[0].requirements.splice(i, 1);
+                    const update_organization = {"name": organization[0].name, "initial_required": organization[0].initial_required, 
+                        "num_boosters": organization[0].num_boosters, "requirements": organization[0].requirements};
+                    await datastore.save({"key": key, "data": update_organization});
+                }
+            }
+        }
+    });
+}
+
+async function delete_requirement(req, id) {
+    const key = datastore.key([REQUIREMENTS, parseInt(id, 10)]);
+    return datastore.delete(key);
 }
 
 /* ------------- End Model Functions ------------- */
@@ -59,20 +107,67 @@ router.get('/:id', function (req, res) {
     });
 });
 
+router.get('/', function (req, res) {
+    get_requirements(req)
+    .then(requirements => {
+        res.status(200).json(requirements);
+    });
+});
+
 router.post('/:id', function (req, res) {
-    if (req.body.days_max === undefined || req.body.days_max === null ||
+    if (req.body.category === undefined || req.body.category === null ||
+        req.body.days_max === undefined || req.body.days_max === null ||
         req.body.days_min === undefined || req.body.days_min === null ||
         req.body.quantity === undefined || req.body.quantity === null ||
         req.body.type === undefined || req.body.type === null) {
             res.status(400).json({'Error': 'The request object is missing at least one of the required attributes'});
     }
     else {
-        post_requirement(req, req.body.days_max, req.body.days_min, req.body.quantity, req.body.type)
+        post_requirement(req, req.body.category, req.body.days_max, req.body.days_min, req.body.quantity, req.body.type)
         .then(requirement => {
             res.status(201).send(requirement);
         });
     }
 });
+
+router.delete('/:id', function (req, res) {
+    get_requirement(req, req.params.id)
+    .then(async requirement => {
+        if (requirement[0] === undefined || requirement[0] === null) {
+            res.status(404).json({'Error': 'No requirement with this requirement ID exists'});
+        } else {
+            var organizations = await get_organizations(req);
+            for (var i = 0; i < organizations.length; i++) {
+                for (var j = 0; j < organizations.organizations[i].requirements.length; j++) {
+                    if (organizations.organizations[i].requirements[j].id == req.params.id) {
+                        await remove_requirement_from_organization(organizations.organizations[i].id, req.params.id);
+                    }
+                }
+            }
+            await delete_requirement(req.params.id);
+            res.status(204).end();
+        }
+    })
+})
+
+router.delete('/:requirement_id/organizations/:organization_id', function (req, res) {
+    get_organization(req, req.params.organization_id)
+    .then(async organization => {
+        if (organization[0] === undefined || organization[0] === null) {
+            res.status(404).json({'Error': 'No organization with this organization ID exists'});
+        } else {
+            get_requirement(req, req.params.id)
+            .then(async requirement => {
+                if (requirement[0] === undefined || requirement[0] === null) {
+                    res.status(404).json({'Error': 'No requirement with this requirement ID exists'});
+                } else {
+                    await remove_requirement_from_organization(req.params.organization_id, req.params.requirement_id);
+                    res.status(204).end();
+                }
+            });
+        }
+    });
+})
 
 /* ------------- End Controller Functions ------------- */
 
